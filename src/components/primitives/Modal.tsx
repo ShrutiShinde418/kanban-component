0e32948a43d6ec97035ef52f6519af1214afa0aa
Shruti Shinde <sss22@somaiya.edu>
2025-10-27 00:59:52 +0530

    style(added modal component and its type)

--- src/components/primitives/Modal.tsx
+++ src/components/primitives/Modal.tsx
@@ -1,5 +1,25 @@
-const Modal = () => {
-  return <dialog></dialog>;
+import { useRef, useEffect, type FC } from "react";
+import { createPortal } from "react-dom";
+import type { ModalProps } from "./types.ts";
+
+const Modal: FC<ModalProps> = ({ children, open, onClose }) => {
+  const dialog = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const modal = dialog.current;
+
+    if (dialog.current) {
+      dialog.current.showModal();
+    }
+
+    return () => modal?.close();
+  }, [open]);
+  return createPortal(
+    <dialog ref={dialog} onClose={onClose}>
+      {children}
+    </dialog>,
+    document.getElementById("modal") as HTMLDivElement,
+  );
 };
 
 export default Modal;
